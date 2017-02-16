3991c49f7a063cc7ac6687fcd848b4ce5cad9476
NothingFiner <e.a.finer@gmail.com>
2017-02-16 16:50:15 -0500

    set up redux pieces for storis

--- app/controllers/api/storis_controller.rb
+++ app/controllers/api/storis_controller.rb
@@ -21,6 +21,7 @@ class Api::StorisController < ApplicationController
 
   def update
     @stori = Stori.find(params[:id])
+    debugger
     if current_user.id == @stori.user_id && @stori.update(stori_params)
       render :show
     else
@@ -30,6 +31,6 @@ class Api::StorisController < ApplicationController
 
   private
   def stori_params
-    params.require(:stori).permit(:title, :author, :content, :metadata, :audio_video, :image_url, :header_image_url, :tags )
+    params.require(:stori).permit(:title, :author, :content, :metadata, { audio_video: :soundcloud, :youtube }, :image_url, :header_image_url, :tags )
   end
 end
---
+++ frontend/actions/storis.js
@@ -0,0 +1,33 @@
+import * as APIStoriUtil from '../util/stori_api';
+import { receiveErrors } from './errors';
+
+export const RECEIVE_STORIS = 'RECEIVE_STORIS';
+export const RECEIVE_STORI = 'RECEIVE_STORI';
+
+export const receiveStoris = storis => ({
+  type: RECEIVE_STORIS,
+  storis,
+});
+
+export const receiveStori = stori => ({
+  type: RECEIVE_STORI,
+  stori,
+});
+
+export const fetchStoris = () => dispatch => (
+  APIStoriUtil.fetchStoris().then(data => dispatch(receiveStoris(data)))
+);
+
+export const fetchStori = storiId => dispatch => (
+  APIStoriUtil.fetchStori(storiId).then(data => dispatch(receiveStori(data)))
+);
+
+export const createStori = stori => dispatch => (
+  APIStoriUtil.createStori(stori)
+    .then(data => dispatch(receiveStori(data)), errors => dispatch(receiveErrors(errors)))
+);
+
+export const updateStori = stori => dispatch => (
+  APIStoriUtil.updateStori(stori)
+    .then(data => dispatch(receiveStori(data)), errors => dispatch(receiveErrors(errors)))
+);
