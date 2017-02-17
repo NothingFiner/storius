1311227ea549b8074d0dcd09657daedafa5d7870
NothingFiner <e.a.finer@gmail.com>
2017-02-17 17:31:51 -0500

    setup for stori form

--- frontend/components/storis/stori_form.jsx
+++ frontend/components/storis/stori_form.jsx
@@ -18,11 +18,31 @@ class StoriForm extends React.Component {
           <h1 className="column header-text">Add Stori</h1>
           <div className="column">
             <form>
-              <ReactQuill
-                value={this.state.text}
-                onChange={this.onTextChange}
-                theme="snow"
-              />
+              <div className="width-full display-flex-between header-label">
+                <h3>Primary Info</h3>
+                <small>*required</small>
+              </div>
+              <div className="form-primary column">
+                <label htmlFor="author">By*</label>
+                <input
+                  id="author"
+                  placeholder="The primary author, artist, or creator"
+                  className="half"
+                  type="text"
+                />
+                <label htmlFor="title">Title*</label>
+                <input id="title" placeholder="Title" className="half" type="text" />
+                <ReactQuill
+                  value={this.state.text}
+                  onChange={this.onTextChange}
+                  theme="snow"
+                />
+              </div>
+              <div className="column">
+                <h3 className="width-full display-flex-between header-label">
+                  Additional Metadata
+                </h3>
+              </div>
             </form>
           </div>
         </section>
