c9f1d52c208db3437e1107783f9582078a315a43
Eric Edward Finer <eric.e.finer@gmail.com>
2017-02-19 13:33:29 -0500

    finish backend setup for annotations

--- app/controllers/api/annotations_controller.rb
+++ app/controllers/api/annotations_controller.rb
@@ -1,2 +1,31 @@
 class Api::AnnotationsController < ApplicationController
+  def create
+    @annotation = Annotation.new(annotation_params)
+    @annotation.user = current_user
+    @annotation.stori_id = params[:stori_id]
+    if @annotation.save
+      render :show
+    else
+      render json: @annotation.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @annotation = Annotation.find(params[:id])
+    if @annotation.update(annotation_params)
+      render :show
+    else
+      render json: @annotation.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @annotation = Annotation.find(params[:id])
+    render :show
+  end
+
+  private
+  def annotation_params
+    params.require(:annotation).permit(:start_idx, :length, :content)
+  end
 end
--- app/controllers/api/storis_controller.rb
+++ app/controllers/api/storis_controller.rb
@@ -1,4 +1,5 @@
 class Api::StorisController < ApplicationController
+  before_action :require_logged_in, only: [:create, :update]
   def index
     @storis = Stori.all
     render :index
