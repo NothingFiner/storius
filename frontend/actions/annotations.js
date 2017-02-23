7ab7b5f47d878d20b931f26b5ad636ce245b9487
Eric Edward Finer <eric.e.finer@gmail.com>
2017-02-22 22:26:39 -0500

    add api util for comments

--- frontend/actions/annotations.js
+++ frontend/actions/annotations.js
@@ -1,6 +1,6 @@
 import { receiveErrors } from './errors';
 import * as APIAnnotationUtil from '../util/annotations_api';
-import { removeStoriAnnotation, updateSelection } from './storis';
+import { removeStoriAnnotation } from './storis';
 
 export const RECEIVE_ANNOTATION = 'RECEIVE_ANNOTATION';
 export const TOGGLE_ANNOTATION = 'TOGGLE_ANNOTATION';
@@ -8,12 +8,10 @@ export const TOGGLE_EDIT = 'TOGGLE_EDIT';
 export const SELECT_ANNOTATION = 'SELECT_ANNOTATION';
 export const REMOVE_ANNOTATION = 'REMOVE_ANNOTATION';
 
-export const receiveAnnotation = annotation => {
-  return {
-    type: RECEIVE_ANNOTATION,
-    annotation,
-  };
-};
+export const receiveAnnotation = annotation => ({
+  type: RECEIVE_ANNOTATION,
+  annotation,
+});
 
 export const toggleAnnotation = annotationId => ({
   type: TOGGLE_ANNOTATION,
